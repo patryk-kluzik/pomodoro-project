f23c99ccc962bad4c1bbc3e976293cd2b3c3aacc
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-01 17:23:11 +0100

    working on the timer

--- main.py
+++ main.py
@@ -20,7 +20,15 @@ CURRENT_STATE = {
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(5)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    canvas.itemconfig(time_canvas, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -32,7 +40,7 @@ window.resizable(False, False)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+time_canvas = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # labels
@@ -46,6 +54,7 @@ tick_mark.grid(column=1, row=3)
 
 # buttons
 start_button = Button(text="Start", activebackground=GREEN, bg="white", bd=0, font=(FONT_NAME, 10, "normal"))
+start_button.config(command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", activebackground=GREEN, bg="white", bd=0, font=(FONT_NAME, 10, "normal"))
